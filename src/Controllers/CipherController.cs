9de030c733370f41c78b244ac1272dd73dfb8d96
Ian Hodges <ian.hodges@hddecisions.co.uk>
2018-10-11 14:38:57 +0100

    Added CMK policy generation.

--- src/Controllers/CipherController.cs
+++ src/Controllers/CipherController.cs
@@ -9,6 +9,9 @@ using Amazon.KeyManagementService;
 using Amazon.KeyManagementService.Model;
 using System.IO;
 using KMSTest.Models;
+using Amazon.SecurityToken;
+using Amazon.SecurityToken.Model;
+using Amazon.Auth.AccessControlPolicy;
 
 namespace KMSTest.Controllers
 {
@@ -52,33 +55,26 @@ namespace KMSTest.Controllers
         [HttpGet("PutKeyPolicy/{keyId}")]
         public ActionResult<PutKeyPolicyResponse> PutKeyPolicy(string keyId)
         {
+            Policy policy;
+
             using (var kms = new AmazonKeyManagementServiceClient())
             {
+                using (AmazonSecurityTokenServiceClient stsClient = new AmazonSecurityTokenServiceClient())
+                {
+                    GetCallerIdentityResponse getCallerIdentityResponse = stsClient.GetCallerIdentityAsync(new GetCallerIdentityRequest()).Result;
+
+                    policy = Help.GetPolicy(
+                        kms.Config.RegionEndpoint.SystemName,
+                        getCallerIdentityResponse.Account,
+                        getCallerIdentityResponse.Arn,
+                        keyId);
+                }
+
                 PutKeyPolicyRequest putKeyPolicyRequest = new PutKeyPolicyRequest()
                 {
                     KeyId = keyId,
                     PolicyName = "default",
-                    Policy = "{" +
-                    "  \"Version\": \"2012-10-17\"," +
-                    "  \"Statement\": [" +
-                    "    {\"Sid\": \"Allow access for ecsRole\"," +
-                    "    \"Effect\": \"Allow\"," +
-                    "    \"Principal\": {\"AWS\": \"arn:aws:iam::313549930986:role/ecsRole\"}," +
-                    "    \"Action\": [" +
-                    "      \"kms:GenerateDataKey\"," +
-                    "      \"kms:Decrypt\"," +
-                    "      \"kms:Encrypt\"" +
-                    "    ]," +
-                    "    \"Resource\": \"*\"}," +
-                    "    {\"Sid\": \"Allow full admin access for root\"," +
-                    "    \"Effect\": \"Allow\"," +
-                    "    \"Principal\": {\"AWS\": \"arn:aws:iam::313549930986:root\"}," +
-                    "    \"Action\": [" +
-                    "      \"kms:*\"" +
-                    "    ]," +
-                    "    \"Resource\": \"*\"}" +
-                    "  ]" +
-                    "}"
+                    Policy = policy.ToJson()
                 };
  
                 PutKeyPolicyResponse putKeyPolicyResponse = kms.PutKeyPolicyAsync(putKeyPolicyRequest).Result;
@@ -120,19 +116,63 @@ namespace KMSTest.Controllers
             return cipher;
         }
     }
+}
 
-    static class Help
+static class Help
+{
+    /// <summary>
+    /// Converts a memory stream to string
+    /// </summary>
+    /// <param name="memStream">The memory stream to convert</param>
+    /// <returns></returns>
+    public static string StreamToString(MemoryStream memStream)
     {
-        /// <summary>
-        /// Converts a memory stream to string
-        /// </summary>
-        /// <param name="memStream">The memory stream to convert</param>
-        /// <returns></returns>
-        public static string StreamToString(MemoryStream memStream)
-        {
-            byte[] bytes = new byte[memStream.Length];
-            memStream.Read(bytes, 0, bytes.Length);
-            return Convert.ToBase64String(bytes);
-       }
+        byte[] bytes = new byte[memStream.Length];
+        memStream.Read(bytes, 0, bytes.Length);
+        return Convert.ToBase64String(bytes);
+    }
+
+    public static Policy GetPolicy(string region, string accountId, string role, string keyId)
+    {
+        Statement statementKeyUser = new Statement(Statement.StatementEffect.Allow);
+        statementKeyUser.Id = "Allow access for EC2 role";
+        statementKeyUser.Principals = new List<Principal>()
+                {
+                    new Principal(role)
+                };
+        statementKeyUser.Actions = new List<ActionIdentifier>()
+                {
+                    new ActionIdentifier("kms:GenerateDataKey"),
+                    new ActionIdentifier("kms:Decrypt"),
+                    new ActionIdentifier("kms:Encrypt")
+                };
+        statementKeyUser.Resources = new List<Resource>
+                {
+                    new Resource($"arn:aws:kms:{region}:{accountId}:key/{keyId}")
+                };
+
+        Statement statementAdmin = new Statement(Statement.StatementEffect.Allow);
+        statementAdmin.Id = "Allow full admin access for root";
+        statementAdmin.Principals = new List<Principal>()
+                {
+                    new Principal($"arn:aws:iam::{accountId}:root")
+                };
+        statementAdmin.Actions = new List<ActionIdentifier>()
+                {
+                    new ActionIdentifier("kms:*"),
+                };
+        statementAdmin.Resources = new List<Resource>
+                {
+                    new Resource($"arn:aws:kms:{region}:{accountId}:key/{keyId}")
+                };
+
+        //TODO - Add a condition?
+        //statement.Conditions.Add(ConditionFactory..NewIpAddressCondition(ipAddress));
+
+        var policy = new Policy();
+        policy.Statements.Add(statementKeyUser);
+        policy.Statements.Add(statementAdmin);
+
+        return policy;
     }
 }
